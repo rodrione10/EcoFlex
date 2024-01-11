5e6ab75877a15516da860eb7db62454ca97b4338
rodrijavi <rodrione1@gmail.com>
2024-01-11 12:34:14 +0100

    Primer commit en la rama Javi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.ecoflex"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ecoflex"
@@ -36,4 +37,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
