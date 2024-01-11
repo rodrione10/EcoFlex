f30a259f642557c79f5098968852e1ee5d795b95
Jaime <jaime.rc3@gmail.com>
2024-01-11 13:32:08 +0100

    Splash creado(con glide)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.ecoflex"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ecoflex"
@@ -36,4 +36,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //Implementar el glide para cargar imagenes
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    //Implementar el swiper para refrescar paginas
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
 }
