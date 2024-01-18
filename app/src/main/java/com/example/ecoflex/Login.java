e6cddeda279dce5efa35c2aa78c3a80dbcb2a070
Jaime <jaime.rc3@gmail.com>
2024-01-18 13:53:55 +0100

    Login Terminado

--- app/src/main/java/com/example/ecoflex/Login.java
+++ app/src/main/java/com/example/ecoflex/Login.java
@@ -2,7 +2,9 @@ package com.example.ecoflex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void openMain(View v){
+        Intent intent = new Intent(Login.this, Main.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+    public void openRegister(View v){
+        Intent intent = new Intent(Login.this, Register.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
