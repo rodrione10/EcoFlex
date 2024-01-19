945df9784d19f2d54092cfd8c2f647d2bea97506
rodrijavi <rodrione1@gmail.com>
2024-01-19 14:00:37 +0100

    Commit mapa ubicacion telefono

--- app/src/main/java/com/example/ecoflex/MapsActivity.java
+++ app/src/main/java/com/example/ecoflex/MapsActivity.java
@@ -1,68 +1,95 @@
 package com.example.ecoflex;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 
 public class MapsActivity extends AppCompatActivity {
     private GoogleMap mMap;
-
-    LatLng sydney = new LatLng(-34, 151);
-    LatLng TamWorth = new LatLng(-31.083332, 150.916672);
-    LatLng NewCastle = new LatLng(-32.916668, 151.750000);
-    LatLng Brisbane = new LatLng(-27.470125, 153.021072);
-    // creating array list for adding all our locations.
+    private final int FINE_PERMISSION_CODE = 1;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
     private ArrayList<LatLng> locationArrayList;
+
+
+    LatLng Mine;
+    // creating array list for adding all our locations.
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-// Comprueba y solicita permisos
-//checkLocationPermission();
-
-//provisional
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
         // in below line we are initializing our array list.
         locationArrayList = new ArrayList<>();
+        locationArrayList.add(new LatLng(-34, 151));
+        locationArrayList.add(new LatLng(-31.083332, 150.916672));
+        locationArrayList.add(new LatLng(-32.916668, 151.750000));
+        locationArrayList.add(new LatLng(-27.470125, 153.021072));
 
-        // on below line we are adding our
-        // locations in our array list.
-        locationArrayList.add(sydney);
-        locationArrayList.add(TamWorth);
-        locationArrayList.add(NewCastle);
-        locationArrayList.add(Brisbane);
 
-        
-        initMap();
+
     }
 
+    private void getLastLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location!= null){
+                    currentLocation=location;
+                    Mine = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                    initMap();
+                }
+            }
+        });
+
+    }
     private void initMap() {
-// Inicializa el mapa
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override public void onMapReady(GoogleMap googleMap)
-            {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
                 mMap = googleMap;
-
 
                 // inside on map ready method
                 // we will be displaying all our markers.
                 // for adding markers we are running for loop and
                 // inside that we are drawing marker on our map.
                 for (int i = 0; i < locationArrayList.size(); i++) {
-
                     // below line is use to add marker to each location of our array list.
                     mMap.addMarker(new MarkerOptions().position(locationArrayList.get(i)).title("Marker"));
 
@@ -72,11 +99,29 @@ public class MapsActivity extends AppCompatActivity {
                     // below line is use to move our camera to the specific location.
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(locationArrayList.get(i)));
                 }
-            }
 
+                // Now, add the marker for the current location (Mine)
+                if (Mine != null) {
+                    mMap.addMarker(new MarkerOptions().position(Mine).title("My Location"));
+                } else {
+                    // Handle the case where Mine is null
+                    Toast.makeText(MapsActivity.this, "Current location not available", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == FINE_PERMISSION_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLastLocation();
+            }else{
+                Toast.makeText(this,"No has concedido la localizacion", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     private BitmapDescriptor
     BitmapFromVector(Context context, int vectorResId)
